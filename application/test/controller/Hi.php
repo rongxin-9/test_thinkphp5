885d834a1c2e1d84856c6499d7ba8d92950fd589
dx <275777697@qq.com>
2022-06-03 00:11:22 +0800

    Revert "Revert "初始信息""
    This reverts commit a0a588f99edb9ba4072a45e5c4f3e2c03df59f48.

---
+++ application/test/controller/Hi.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\test\controller;
+
+class HI
+{
+    public function yunzhier()
+    {
+        return '<br><div align="center"> <h1 style="color:red "> hi yunzhier！！！</h1></div>';
+    }
+    public function luck()
+    {
+        return 'Luck here!';
+    }
+}
